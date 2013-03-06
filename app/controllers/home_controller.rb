ec8990bfff20befb6443661b454b379735012994
Roberto Bartolome <roberto.bartolome.medina@gmail.com>
2013-03-06 16:26:45 +0100

    WIP

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
   def index
     @users = User.all
-    
+        
     @h = LazyHighCharts::HighChart.new('graph', style: '') do |f|
       f.options[:chart][:defaultSeriesType] = "area"
       f.options[:plotOptions] = {areaspline: {pointInterval: 1.day, pointStart: 10.days.ago}}
@@ -9,5 +9,44 @@ class HomeController < ApplicationController
       f.series(:name=>'Jane', :data=> [1, 3, 4, 3, 3, 5, 4,-46,7,8,8,9,9,0,0,9])
       f.xAxis(type: :datetime)
     end
+
+    @chart = LazyHighCharts::HighChart.new('pie') do |f|
+          f.chart({:defaultSeriesType=>"pie" , :margin=> [50, 200, 60, 170]} )
+          series = {
+                   :type=> 'pie',
+                   :name=> 'Browser share',
+                   :data=> [
+                      ['Firefox',   45.0],
+                      ['IE',       26.8],
+                      {
+                         :name=> 'Chrome',    
+                         :y=> 12.8,
+                         :sliced=> true,
+                         :selected=> true
+                      },
+                      ['Safari',    8.5],
+                      ['Opera',     6.2],
+                      ['Others',   0.7]
+                   ]
+          }
+          f.series(series)
+          f.options[:title][:text] = "THA PIE"
+          f.legend(:layout=> 'vertical',:style=> {:left=> 'auto', :bottom=> 'auto',:right=> '50px',:top=> '100px'}) 
+          f.plot_options(:pie=>{
+            :allowPointSelect=>true, 
+            :cursor=>"pointer" , 
+            :dataLabels=>{
+              :enabled=>true,
+              :color=>"black",
+              :style=>{
+                :font=>"13px Trebuchet MS, Verdana, sans-serif"
+              }
+            }
+          })
+    end
+    
+    
+    
+    
   end
 end
